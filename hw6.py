a343c0123bed45bbe8294180c0abb29e1b03ffdd
jeonseungyong <127211588+jeonseungyong@users.noreply.github.com>
2023-04-21 21:32:44 +0900

    hw6.py

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for n in range (1,10):
+  for i in range(2,6):
+   print(f'{i}*{n}={n*i:2d}',end='\t')
+  print()
+
+print()
+
+for n in range (1,10):
+  for i in range(6,10):
+   print(f'{i}*{n}={n*i:2d}',end='\t')
+  print()
